2490ae6e4d7bf32cb01eec43687942daa59af845
Mohannad Banayosi <m.banayosi@gmail.com>
2014-01-17 17:54:53 +0200

    Prefetch added

--- matrix_multiplication_921/matrixMultiply.cu
+++ matrix_multiplication_921/matrixMultiply.cu
@@ -174,10 +174,6 @@ inline __global__ void MatrixMulKernelTiled16x16(float* Md, float* Nd, float* Pd
 	Pd[Row*Width + Col] = Pvalue; 
 }
 
-inline __global__ void MatrixMulKernelTiled8x8Prefetching(float* Md, float* Nd, float* Pd, int Width) { 
-
-}
-
 inline __global__ void MatrixMulKernelTiledUnrolling(float* Md, float* Nd, float* Pd, int Width, int unrolling) { 
 	const int TILE_WIDTH = 8;
 	__shared__ float Mds[TILE_WIDTH][TILE_WIDTH]; 
@@ -226,6 +222,36 @@ inline __global__ void MatrixMulKernelTiledUnrolling(float* Md, float* Nd, float
 	Pd[Row*Width + Col] = Pvalue; 
 }
 
+inline __global__ void MatrixMulKernelTiled8x8prefetch(float* Md, float* Nd, float* Pd, int Width) { 
+	const int TILE_WIDTH = 8;
+	__shared__ float Mds[TILE_WIDTH][TILE_WIDTH]; 
+	__shared__ float Nds[TILE_WIDTH][TILE_WIDTH]; 
+	int bx = blockIdx.x;  
+	int by = blockIdx.y; 
+	int tx = threadIdx.x; 
+	int ty = threadIdx.y; 
+	// Identify the row and column of the Pd element to work on
+	int Row = by * TILE_WIDTH + ty; 
+	int Col = bx * TILE_WIDTH + tx; 
+	float Pvalue = 0; 
+	// Loop over the Md and Nd tiles required to compute the Pd element 
+
+	int M = Md[Row*Width + (0*TILE_WIDTH + tx)]; 
+	int N = Nd[(0*TILE_WIDTH + ty)*Width + Col];
+	for (int m = 1; m < Width/TILE_WIDTH; ++m) { 
+		Mds[ty][tx] = M;
+		Nds[ty][tx] = N;
+		__syncthreads();
+
+		M = Md[Row*Width + (m*TILE_WIDTH + tx)]; 
+		N = Nd[(m*TILE_WIDTH + ty)*Width + Col];
+		
+		for (int k = 0; k < TILE_WIDTH; ++k) 
+			Pvalue += Mds[ty][k] * Nds[k][tx];
+		__syncthreads();
+	} 
+	Pd[Row*Width + Col] = Pvalue; 
+}
 
 inline __global__ void MatrixMulKernelTiled8x8gran1x2(float* Md, float* Nd, float* Pd, int Width) { 
 	const int TILE_WIDTH = 8;
